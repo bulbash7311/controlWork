c15e122ce1024c26c01b23e68265f55dcce5ae4b
bulbash <mdv73rus@yandex.ru>
2023-09-24 11:34:35 +0400

    added program.cs

---
+++ task/Program.cs
@@ -0,0 +1,25 @@
+﻿
+// Ввод исходного массива строк
+Console.WriteLine("Введите элементы массива через запятую:");
+string[] inputArray = Console.ReadLine().Split(',');
+
+// Создание нового массива для строк длиной <= 3 символам
+string[] newArray = new string[inputArray.Length];
+int newIndex = 0;
+
+// Проверка каждой строки и добавление в новый массив, если длина <= 3
+for (int i = 0; i < inputArray.Length; i++)
+{
+    if (inputArray[i].Length <= 3)
+    {
+        newArray[newIndex] = inputArray[i];
+        newIndex++;
+    }
+}
+
+// Вывод нового массива
+Console.WriteLine("Новый массив:");
+for (int i = 0; i < newIndex; i++)
+{
+    Console.WriteLine(newArray[i]);
+}
